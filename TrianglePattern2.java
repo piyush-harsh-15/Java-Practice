1af599920d8e0a21ba6325b13c646f61ef4bbde4
Piyush Harsh <76912417+piyush-harsh-15@users.noreply.github.com>
2021-11-29 22:18:12 +0530

    commit

---
+++ TrianglePattern2.java
@@ -0,0 +1,13 @@
+class TrianglePattern2 {
+    public static void main(String[] args) {
+        int i, j;
+        int n = 6;
+        for (i = n; i > 0; i--) {
+            for (j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+
+    }
+}
